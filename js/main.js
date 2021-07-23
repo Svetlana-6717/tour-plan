a4603da6edb6c16d0634e57600fe22e2e3046e02
Svetlana <sl1248@yandex.ru>
2021-07-23 20:48:05 +0300

    create: modal block

--- js/main.js
+++ js/main.js
@@ -1,38 +1,71 @@
-'use strict';
+window.addEventListener('DOMContentLoaded', function () {
 
-const hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+  'use strict';
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+  // slider
 
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
+  const hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
 
-const menuButton = document.querySelector('.menu-button');
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
 
-menuButton.addEventListener('click', () => {
-  const toggleMenu = document.querySelector('.navbar-bottom');
-  toggleMenu.classList.toggle('navbar-bottom--visible');
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+
+  // menu
+
+  const menuButton = document.querySelector('.menu-button');
+
+  menuButton.addEventListener('click', () => {
+    const toggleMenu = document.querySelector('.navbar-bottom');
+    toggleMenu.classList.toggle('navbar-bottom--visible');
+  });
+
+  // modal block
+
+  const toggleModal = () => {
+    const modalOverlay = document.querySelector('.modal__overlay');
+    const modalDialog = document.querySelector('.modal__dialog');
+    const modalButton = document.querySelector('.booking__button');
+    const closeModalButton = document.querySelector('.modal__close');
+
+    const handlerModal = () => {
+      modalOverlay.classList.toggle('modal__overlay--visible');
+      modalDialog.classList.toggle('modal__dialog--visible');
+    };
+
+    modalButton.addEventListener('click', () => {
+      handlerModal();
+    });
+
+    closeModalButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      handlerModal();
+    });
+
+  };
+
+  toggleModal();
 });
